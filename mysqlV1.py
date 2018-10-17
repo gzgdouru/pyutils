c4dd5e0f2cd013318166808fcc0b16fac5eec602
ouru <18719091650@163.com>
2018-10-17 11:51:00 +0800

    modify

--- mysqlV1.py
+++ mysqlV1.py
@@ -1,6 +1,7 @@
 import pymysql
 from DBUtils.PooledDB import PooledDB
 from collections import namedtuple
+from datetime import datetime
 
 
 class Singleton(object):
@@ -20,7 +21,8 @@ class MysqlManager(Singleton):
 
     def __init__(self, host="localhost", port=3306, user="root", passwd="123456", db="mysql", charset="utf8",
                  max_overflow=10):
-        MysqlManager.pool = PooledDB(creator=pymysql, maxconnections=max_overflow, host=host, port=port, user=user, passwd=passwd,
+        MysqlManager.pool = PooledDB(creator=pymysql, maxconnections=max_overflow, host=host, port=port, user=user,
+                                     passwd=passwd,
                                      db=db, charset=charset, cursorclass=pymysql.cursors.DictCursor)
 
     @staticmethod
@@ -50,11 +52,56 @@ class MysqlManager(Singleton):
             keys.append(key)
             values.append("'{0}'".format(value) if type(value) not in [int, float] else str(value))
         sql = "insert into {0}({1}) values({2})".format(table, ",".join(keys), ",".join(values))
-        MysqlManager.execute(sql)
+
+        conn = MysqlManager.pool.connection()
+        cur = conn.cursor()
+        rows = 0
+
+        try:
+            rows = cur.execute(sql)
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            raise RuntimeError("sql[{0}]执行出错:{1}".format(sql, str(e)))
+        finally:
+            conn.close()
+            cur.close()
+        return rows
+
+    @staticmethod
+    def select(table, conditions=None, order_by=None):
+        sql = "select * from {table}".format(table=table)
+
+        if conditions:
+            sql = "{query} where {conditions}".format(query=sql, conditions=conditions)
+
+        if order_by:
+            order_by = order_by if order_by[0] != "-" else "{0} desc".format(order_by[1:])
+            sql = "{query} order by {order_by}".format(query=sql, order_by=order_by)
+
+        yield from MysqlManager.execute(sql)
+
+    @staticmethod
+    def count(table, conditions=None):
+        sql = "select count(1) as nums from {table}".format(table=table)
+        if conditions:
+            sql = "{query} where {conditions}".format(query=sql, conditions=conditions)
+        result = MysqlManager.execute(sql)
+        for r in result:
+            return r.nums
+        return 0
+
+    @staticmethod
+    def exist(table, conditions):
+        sql = "select 1 as is_exist from {table} where {conditions} limit 1".format(table=table, conditions=conditions)
+        result = MysqlManager.execute(sql)
+        for r in result:
+            return True
+        return False
 
 
 if __name__ == "__main__":
     mysqldb = MysqlManager(host="localhost")
-    sql = "select * from student"
-    result = MysqlManager.execute(sql)
-    print(next(result).id)
+    rows = mysqldb.select("student")
+    for row in rows:
+        pass
